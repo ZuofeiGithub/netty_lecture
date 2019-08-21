f160ee3d94b8f6b1d5a1b0fc247d1f3423227127
zuofei <348068347@qq.com>
2019-08-21 15:53:01 +0800

    Initial commit

---
+++ src/main/java/com/zuofei/netty/firstexample/TestServer.java
@@ -0,0 +1,31 @@
+package com.zuofei.netty.firstexample;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * @content:
+ * @auther: 左飞
+ * @date: 2019/8/21 14:51
+ */
+public class TestServer {
+    public static void main(String[] args) throws InterruptedException {
+        //事件循环组
+        EventLoopGroup boss = new NioEventLoopGroup(); //异步io,就是一个死循环 获取连接，boos接收到连接之后移交给worker
+        EventLoopGroup worker = new NioEventLoopGroup();
+
+        try {
+
+          ServerBootstrap serverBootstrap = new ServerBootstrap();
+          serverBootstrap.group(boss,worker).channel(NioServerSocketChannel.class).childHandler(new TestServerInitializer());
+          ChannelFuture channelFuture = serverBootstrap.bind(8899).sync();
+          channelFuture.channel().closeFuture().sync();
+      }finally {
+            boss.shutdownGracefully();
+            worker.shutdownGracefully();
+      }
+    }
+}
---
+++ src/main/java/com/zuofei/netty/firstexample/TestServerInitializer.java
@@ -0,0 +1,20 @@
+package com.zuofei.netty.firstexample;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
+
+/**
+ * @content:
+ * @auther: 左飞
+ * @date: 2019/8/21 15:00
+ */
+public class TestServerInitializer  extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+        pipeline.addLast("httpServerCodec",new HttpServerCodec());
+        pipeline.addLast("testHttpServerHandler",new TestHttpServerHandler());
+    }
+}
